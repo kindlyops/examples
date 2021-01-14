c3051845d6021df7bee5682441ca2f0d1b93fda4
Elliot Murphy <statik@users.noreply.github.com>
2021-01-13 23:36:32 -0500

    Go mod containing  an updateable dep with +incompatible
    Signed-off-by: Elliot Murphy <statik@users.noreply.github.com>

--- jwt1/main.go
+++ jwt1/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/dgrijalva/jwt-go"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+
+	// example code for custom claims
+	// https://godoc.org/github.com/dgrijalva/jwt-go#example-NewWithClaims--CustomClaimsType
+	mySigningKey := []byte("AllYourBase")
+
+	type MyCustomClaims struct {
+		Foo string `json:"foo"`
+		jwt.StandardClaims
+	}
+
+	// Create the Claims
+	claims := MyCustomClaims{
+		"bar",
+		jwt.StandardClaims{
+			ExpiresAt: 15000,
+			Issuer:    "test",
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	ss, err := token.SignedString(mySigningKey)
+	fmt.Printf("%v %v\n", ss, err)
 }
